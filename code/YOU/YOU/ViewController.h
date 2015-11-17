fb90947af7edd39d26e8457bc91fef00da13947e
weishouqiang <weishouqaing@epailive.com>
2015-11-17 14:47:23 +0800

    init

---
+++ code/YOU/YOU/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YOU
+//
+//  Created by Shouqiang Wei on 15/11/17.
+//  Copyright (c) 2015年 YOU: Say to yourself. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ code/YOU/YOU/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YOU
+//
+//  Created by Shouqiang Wei on 15/11/17.
+//  Copyright (c) 2015年 YOU: Say to yourself. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
